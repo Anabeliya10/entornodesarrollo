f090b5767a4bf8c65cbd4d03d1eb109587c19360
Profe <Profe@DESKTOP-8OVFM6T>
2023-04-20 19:25:14 +0200

    commit message

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -5,6 +5,9 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Estamos haciendo control de las versiones con Git");
+	System.out.println("Hola mundo");
+	
 	}
+	
 
 }
